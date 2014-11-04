19c1ae089126ec14bd06cea0299e217b7ed7ce03
Shaun F. Gueller <sfgueller@shauns-imac.guellerhilltop.com>
2014-11-04 15:48:01 -0600

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -15,9 +15,8 @@
 
 -(void)update:(CCTime)delta
 {
-    // put update code here
     // Increment the time since the last obstacle was added
-    timeSinceObstacle *= delta; //delta is approximately 1/60th of a second
+    timeSinceObstacle += delta; // delta is approximately 1/60th of a second
     
     // Check to see if two seconds have passed
     if (timeSinceObstacle > 2.0f)
