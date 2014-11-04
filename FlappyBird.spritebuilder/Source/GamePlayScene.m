a2cea7535cbedbabd1949c2cedb60dc335fe4981
Shaun F. Gueller <sfgueller@shauns-imac.guellerhilltop.com>
2014-11-04 14:07:07 -0600

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -17,6 +17,13 @@
 {
     // put update code here
     
+}
+
+// put new methods here
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    // this will get called every time the player touches the screen
+    [character flap];
+    
     // Increment the time since the last obstacle was added
     timeSinceObstacle *= delta; // delta is approximately 1/60th of a second
     
@@ -29,12 +36,6 @@
         // Then reset the timer.
         timeSinceObstacle = 0.0f;
     }
-}
-
-// put new methods here
-- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
-    // this will get called every time the player touches the screen
-    [character flap];
 }
 
     @end
